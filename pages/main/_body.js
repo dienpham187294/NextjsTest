a3dc32fb03da3259083f608e01c8b04d0ddc0c05
Dienpham <dienpham187294@gmail.com>
2021-05-25 15:54:50 +0700

    fix some css

--- pages/main/_body.js
+++ pages/main/_body.js
@@ -8,16 +8,16 @@ export default function body() {
     return (
         <>
             <main className={styles.main}>
-                <h1 className={styles.title}>
-                    Welcome to <a href="#">ESL GAME!</a>
-                </h1>
-                <br />
-                <h4> Nền tảng trò chơi thực hành tiếng anh giao tiếp!</h4>
+                <div style={{ textAlign: "center" }}>
+                    <h1 className={styles.title}>
+                        Welcome to <a href="#">ESL GAME!</a>
+                    </h1>
+                    <br />
+                    <h4> Nền tảng trò chơi thực hành tiếng anh giao tiếp!</h4>
 
-                <i style={{ textAlign: "center" }}>Ứng dụng công nghệ nhận diện giọng nói mang đến trải nghiệm thực hành tiếng anh thực sự thú vị và hiệu quả.</i>
+                    <i >Ứng dụng công nghệ nhận diện giọng nói mang đến trải nghiệm thực hành tiếng anh thực sự thú vị và hiệu quả.</i>
 
-
-
+                </div>
 
 
             </main>
