ac81b79980318ba531a951164e23bf16102c551e
Dienpham <dienpham187294@gmail.com>
2021-06-16 20:13:54 +0700

    cors fixx

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,9 +1,14 @@
 
-
+import NextCors from 'nextjs-cors';
 const gse = require("general-search-engine")
 
 export default async (req, res) => {
-
+    await NextCors(req, res, {
+        // Options
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
     // console.log(req.body.Word)
 
     try {
