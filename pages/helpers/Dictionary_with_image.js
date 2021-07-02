9c3e701138e2ba919b11219f41376ca9f7089065
Dienpham <dienpham187294@gmail.com>
2021-07-02 16:36:22 +0700

    a

--- pages/helpers/Dictionary_with_image.js
+++ pages/helpers/Dictionary_with_image.js
@@ -3,33 +3,38 @@ import GetLongest from "../../util/GetLongest"
 import ImageSearch from "./ImageSearch"
 
 function Dictionary_with_image(props) {
-    return (
-        props.Popup !== "" ?
-            <div
-                style={{
-                    zIndex: 2,
-                    position: "fixed",
-                    left: "2%",
-                    top: "2%",
-                    border: "1px solid green",
-                    borderRadius: "10px",
-                    padding: "15px",
-                    width: "20%",
-                    minWidth: "250px",
-                    maxHeight: "100%",
-                    backgroundColor: "white",
-                    overflow: "auto"
-                }}
-            >
-                {props.Popup}
-                <hr />
-                <Dictionary Word={GetLongest(props.Popup)} />
-                <hr />
-                < ImageSearch Word={props.Popup} />
-                <hr />
-            </div>
-            : null
-    )
+    try {
+        return (
+            props.Popup !== "" ?
+                <div
+                    style={{
+                        zIndex: 2,
+                        position: "fixed",
+                        left: "2%",
+                        top: "2%",
+                        border: "1px solid green",
+                        borderRadius: "10px",
+                        padding: "15px",
+                        width: "20%",
+                        minWidth: "250px",
+                        maxHeight: "100%",
+                        backgroundColor: "white",
+                        overflow: "auto"
+                    }}
+                >
+                    {props.Popup}
+                    <hr />
+                    <Dictionary Word={GetLongest(props.Popup)} />
+                    <hr />
+                    < ImageSearch Word={props.Popup} />
+                    <hr />
+                </div>
+                : null
+        )
+    } catch (error) {
+        return null
+    }
+
 }
 
 export default Dictionary_with_image
