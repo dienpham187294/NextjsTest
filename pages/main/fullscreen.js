0d3b4d5797e586035adef672be16d90b253d8b9b
Dienpham <dienpham187294@gmail.com>
2021-07-03 09:17:49 +0700

    change full sreen

--- pages/main/fullscreen.js
+++ pages/main/fullscreen.js
@@ -4,8 +4,14 @@ export default function Fullsreen() {
     return (
         <div id="fullsreen">
             <button onClick={() => {
-                var videoElement = document.getElementById("fullsreen");
-                videoElement.requestFullscreen();
+                let elem = document.getElementById("fullsreen");
+                if (elem.requestFullScreen) {
+                    elem.requestFullScreen();
+                } else if (elem.mozRequestFullScreen) {
+                    elem.mozRequestFullScreen();
+                } else if (elem.webkitRequestFullScreen) {
+                    elem.webkitRequestFullScreen();
+                }
             }}>
                 requestFullscreenc
             </button>
