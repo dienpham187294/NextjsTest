58046c2c2fd5b35adefe0a828369ee6ec13c0222
Dienpham <dienpham187294@gmail.com>
2021-07-07 17:43:19 +0700

    8

--- util/Cookies/functionCookies.js
+++ util/Cookies/functionCookies.js
@@ -1,10 +1,13 @@
 function setCookie(cname, cvalue, exdays) {
 
     try {
-        var d = new Date();
-        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-        var expires = "expires=" + d.toUTCString();
-        document.cookie = cname + "=" + cvalue + "; " + expires;
+        if (process.browser) {
+            var d = new Date();
+            d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+            var expires = "expires=" + d.toUTCString();
+            document.cookie = cname + "=" + cvalue + "; " + expires;
+        }
+
     } catch (error) {
         console.log(error)
     }
@@ -19,18 +22,20 @@ function delettCookie(cname) {
 
 function getCookie(cname) {
     try {
-        var name = cname + "=";
-        var ca = document.cookie.split(';');
-        for (var i = 0; i < ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) === ' ') {
-                c = c.substring(1);
+        if (process.browser) {
+            var name = cname + "=";
+            var ca = document.cookie.split(';');
+            for (var i = 0; i < ca.length; i++) {
+                var c = ca[i];
+                while (c.charAt(0) === ' ') {
+                    c = c.substring(1);
+                }
+                if (c.indexOf(name) === 0) {
+                    return c.substring(name.length, c.length);
+                }
             }
-            if (c.indexOf(name) === 0) {
-                return c.substring(name.length, c.length);
-            }
+            return "";
         }
-        return "";
     } catch (error) {
         console.log(error)
     }
