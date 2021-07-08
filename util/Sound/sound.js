5becd654ed737516a4eb62100a0c28c1fcddcb11
Dienpham <dienpham187294@gmail.com>
2021-07-08 18:05:33 +0700

    aa

--- util/Sound/sound.js
+++ util/Sound/sound.js
@@ -2,15 +2,23 @@ import { async } from "regenerator-runtime";
 
 export default async function Sound(str) {
     try {
-        var x = await document.createElement("AUDIO");
-        x.setAttribute("class", "soundClass")
-        if (x.canPlayType("audio/mpeg")) {
-            x.setAttribute("src", "/sound/" + str);
+        
+        if (document.getElementById(str) !== null) {
+            document.getElementById(str).play();
+           
         } else {
-            x.setAttribute("src", "/sound/" + str);
+            
+            let x = await document.createElement("AUDIO");
+            x.setAttribute("class", "soundClass")
+            x.setAttribute("id", str)
+            if (x.canPlayType("audio/mpeg")) {
+                x.setAttribute("src", "/sound/" + str);
+            } else {
+                x.setAttribute("src", "/sound/" + str);
+            }
+            document.getElementById("sound").appendChild(x);
+            x.play()
         }
-        document.getElementById("sound").appendChild(x);
-        x.play()
     } catch (error) {
         console.log("e")
     }
