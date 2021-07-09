da20646bb82e1cfd966fa08984a4396d98275e99
Dienpham <dienpham187294@gmail.com>
2021-07-09 22:02:08 +0700

    adđ

--- pages/main/testios.js
+++ pages/main/testios.js
@@ -46,58 +46,58 @@ export default function tets(params) {
     }, [])
     return (
         <div>
-            <div class="container">
+            <div className="container">
                 <hr />
                 <button onClick={() => {
                     arr++
                 }}>Cộng lên</button>
-                <div class="row">
+                <div className="row">
                     <nav>
-                        <div class="nav-wrapper">
-                            <div class="col s12">
-                                <a href="#" class="brand-logo">Text to speech example</a>
+                        <div className="nav-wrapper">
+                            <div className="col s12">
+                                <a href="#" className="brand-logo">Text to speech example</a>
                             </div>
                         </div>
                     </nav>
                 </div>
-                <form class="col s8 offset-s2">
-                    <div class="row">
+                <form className="col s8 offset-s2">
+                    <div className="row">
                         <label>Choose voice</label>
                         <select id="voices"></select>
                     </div>
-                    <div class="row">
-                        <div class="col s6">
+                    <div className="row">
+                        <div className="col s6">
                             <label>Rate</label>
-                            <p class="range-field">
+                            <p className="range-field">
                                 <input type="range" id="rate" min="1" max="100" value="10" />
                             </p>
                         </div>
-                        <div class="col s6">
+                        <div className="col s6">
                             <label>Pitch</label>
-                            <p class="range-field">
+                            <p className="range-field">
                                 <input type="range" id="pitch" min="0" max="2" value="1" />
                             </p>
                         </div>
-                        <div class="col s12">
+                        <div className="col s12">
                             <p>N.B. Rate and Pitch only work with native voice.</p>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <textarea id="message" class="materialize-textarea"></textarea>
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <textarea id="message" className="materialize-textarea"></textarea>
                             <label>Write message</label>
                         </div>
                     </div>
-                    <a href="#" id="speak" class="waves-effect waves-light btn">Speak</a>
+                    <a href="#" id="speak" className="waves-effect waves-light btn">Speak</a>
                 </form>
             </div>
 
-            <div id="modal1" class="modal">
+            <div id="modal1" className="modal">
                 <h4>Speech Synthesis not supported</h4>
                 <p>Your browser does not support speech synthesis.</p>
                 <p>We recommend you use Google Chrome.</p>
-                <div class="action-bar">
-                    <a href="#" class="waves-effect waves-green btn-flat modal-action modal-close">Close</a>
+                <div className="action-bar">
+                    <a href="#" className="waves-effect waves-green btn-flat modal-action modal-close">Close</a>
                 </div>
             </div>
         </div>
