2a24214e8f5efd72aa2a62b002c0fc153328a91c
Dienpham <dienpham187294@gmail.com>
2021-07-13 08:28:18 +0700

    Nex git

--- pages/thuchanh/dung-thu/10-cau-thong-dung-nhat.js
+++ pages/thuchanh/dung-thu/10-cau-thong-dung-nhat.js
@@ -1,8 +1,7 @@
-import Jsonfile_input from "../../../util/App/LEARN_SENTENC_Basic/Demo_Sentence_Basic_Data"
 import Show_Demo_Sentence_Basic from "../../../util/App/LEARN_SENTENC_Basic/Show_Demo_Sentence_Basic"
 export default function Index_10cauthongdungnhat() {
     return (
-        Show_Demo_Sentence_Basic(Jsonfile_input["5-cau-co-ban-nhat"])
+        Show_Demo_Sentence_Basic()
     )
 }
 
