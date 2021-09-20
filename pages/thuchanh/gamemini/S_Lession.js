bce055cd8466a6426845a3d4ab61ec5d2d673cf3
Dienpham <dienpham187294@gmail.com>
2021-09-20 17:06:42 +0700

    add 1

--- pages/thuchanh/gamemini/S_Lession.js
+++ pages/thuchanh/gamemini/S_Lession.js
@@ -7,53 +7,61 @@ export default function Lesson(props) {
     const [Data_Learn, SET_Data_Learn] = useState("")
     const [Docthu, SET_Docthu] = useState("")
     const [Data_Commands, SET_Data_Commands] = useState("====")
-    return (
-        <div >
-            {Data_Learn !== "" ? Show_tienganhphothong_phanbaihoc(Data_Learn, Docthu, SET_Docthu, SET_Data_Commands, Data_Commands) : null}
-            {props.Data.map((e, i) =>
+
+    try {
+        return (
+            <div >
+                {Data_Learn !== "" ? Show_tienganhphothong_phanbaihoc(Data_Learn, Docthu, SET_Docthu, SET_Data_Commands, Data_Commands) : null}
+                {props.Data.map((e, i) =>
+                    <div
+                        onClick={() => {
+                            SET_Data_Learn(e);
+                            document.getElementById("idStartLisening").click();
+                        }}
+                        key={i}
+                        style={{ border: "1px solid green", borderRadius: "5px", padding: "5px", margin: "5px", cursor: "pointer" }}
+                    >
+                        <span style={{ color: "blue" }}> {e.EN}</span>
+                        <br />
+                        <i>{e.VN}</i>
+                        <br />
+                        <button style={{ backgroundColor: "yellow" }}>Bấm chọn để học</button>
+
+                    </div>
+                )}
+
                 <div
-                    onClick={() => {
-                        SET_Data_Learn(e);
-                        document.getElementById("idStartLisening").click();
+                    style={{
+                        position: "fixed",
+                        bottom: "10%",
+                        right: "10%",
+                        width: "10%",
+                        minWidth: "200px",
+                        backgroundColor: "white"
                     }}
-                    key={i}
-                    style={{ border: "1px solid green", borderRadius: "5px", padding: "5px", margin: "5px", cursor: "pointer" }}
                 >
-                    <span style={{ color: "blue" }}> {e.EN}</span>
-                    <br />
-                    <i>{e.VN}</i>
-                    <br />
-                    <button style={{ backgroundColor: "yellow" }}>Bấm chọn để học</button>
+                    <button
+                        className="btn btn-outline-danger form-control mt-1"
+                        onClick={() => {
+                            props.Total.fnObj.SET_PageChange(0);
+                            try {
+                                $("#idStopLisening").click()
+                            } catch (error) {
 
+                            }
+                        }}>Trở lại phần chọn bài</button>
                 </div>
-            )}
 
-            <div
-                style={{
-                    position: "fixed",
-                    bottom: "10%",
-                    right: "10%",
-                    width: "10%",
-                    minWidth: "200px",
-                    backgroundColor: "white"
-                }}
-            >
-                <button
-                    className="btn btn-outline-danger form-control mt-1"
-                    onClick={() => {
-                        props.Total.fnObj.SET_PageChange(0);
-                        try {
-                            $("#idStopLisening").click()
-                        } catch (error) {
-
-                        }
-                    }}>Trở lại phần chọn bài</button>
+                <ReadReactSpeech />
+                <Dictaphone Data={Data_Commands} Xuly={Xuly} />
             </div>
+        )
+    } catch (error) {
+        return null
+    }
 
-            <ReadReactSpeech />
-            <Dictaphone Data={Data_Commands} Xuly={Xuly} />
-        </div>
-    )
+
+
 
     function Xuly() {
         if (Docthu === "Docthu") {
