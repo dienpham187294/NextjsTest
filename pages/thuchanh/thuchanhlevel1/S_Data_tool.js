35202bed27a9d69ec07f9b9953d1d948815c7aae
Dienpham <dienpham187294@gmail.com>
2021-10-05 15:32:57 +0700

    add

--- pages/thuchanh/thuchanhlevel1/S_Data_tool.js
+++ pages/thuchanh/thuchanhlevel1/S_Data_tool.js
@@ -1,18 +1,12 @@
-// import { useEffect, useState } from "react"
-
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 function DataTool(props) {
-    useEffect((
-    ) => {
-        console.log(props.Data, "2222")
-    },
-        [])
-
+    const [Data_phongto, SET_Data_phongto] = useState(null)
     try {
         return (
             <>
-                {Show_Table(props.Data, props.Total)}
+                {Show_Table(props.Data, props.Total, SET_Data_phongto)}
+                {ShowDataPhongto(Data_phongto, props.Total, SET_Data_phongto)}
             </>
         )
     } catch (error) {
@@ -23,15 +17,15 @@ function DataTool(props) {
 export default DataTool
 
 
-function Show_Table(Data, Total) {
+function Show_Table(Data, Total, SET_Data_phongto) {
     try {
         return Data.map((e, index) =>
             <div key={index} style={{ maxHeight: "250px", overflow: "auto" }}>
                 <div >
                     {e.dataoftable.map((eee, indexeee) =>
+
                         <div
                             key={indexeee}
-
                             style={{
                                 display: "inline-block",
                                 cursor: "pointer",
@@ -41,6 +35,10 @@ function Show_Table(Data, Total) {
                                 maxHeight: "150px",
                                 overflowY: "auto"
                             }}>
+                            <button
+                                className="btn btn-sm btn-outline-primary"
+                                onClick={() => { SET_Data_phongto(eee) }}
+                            >Phóng to</button>
                             {eee.map((eeee, indexeeee) =>
                                 <div
                                     key={indexeeee}
@@ -65,10 +63,15 @@ function Show_Table(Data, Total) {
                                     }
                                     < br />
                                 </div>
+
                             )}
                         </div>
+
+
+
                     )}
                 </div>
+
             </div >
         )
 
@@ -90,4 +93,63 @@ function Check_ImageOrNot(Stringtocheck) {
         ) { return true }
     }
     return false
+}
+function ShowDataPhongto(data, Total, SET_Data_phongto) {
+    try {
+
+        return (
+            <div
+                style={{
+                    position: "fixed",
+                    border: "1px solid green",
+                    borderRadius: "4px",
+                    top: "5px",
+                    bottom: "5px",
+                    left: "5px",
+                    right: "5px",
+                    backgroundColor: "white",
+                    overflow: "auto",
+                    zIndex: 3
+                }}>
+                <button
+                    className="btn btn-danger"
+                    onClick={() => { SET_Data_phongto(null) }}
+                    style={{ position: "fixed", top: "5px", right: "20%" }}
+                >Trở lại</button>
+                {
+                    data.map((eeee, indexeeee) =>
+                        <div
+                            key={indexeeee}
+                        >
+
+                            {Check_ImageOrNot(eeee) ?
+                                <img
+                                    onClick={() => {
+                                        Total.stObj.inputSumit += "===" + eeee;
+                                        // console.log(eeee)
+                                        Total.fnObj.Submit_Show_OnePeopeAppear_ReactData(Total.stObj.inputSumit)
+                                        // console.log(eee[e.indexToSubmit])
+                                    }}
+                                    src={eeee} width="350px" style={{ margin: "5px 25px", cursor: "pointer" }} /> :
+                                <span
+                                    onClick={() => {
+                                        Total.stObj.inputSumit += "===" + eeee;
+                                        // console.log(eeee)
+                                        Total.fnObj.Submit_Show_OnePeopeAppear_ReactData(Total.stObj.inputSumit)
+                                        // console.log(eee[e.indexToSubmit])
+                                    }}
+                                    style={{ cursor: "pointer" }}
+                                >{eeee}</span>
+                            }
+                            < br />
+                        </div>
+                    )
+                }
+            </div>
+
+        )
+
+    } catch (error) {
+        return null
+    }
 }
--- util/filedulieu1/DulieuThuchanhLv1/DulieuTotal.js
+++ util/filedulieu1/DulieuThuchanhLv1/DulieuTotal.js
@@ -1,11 +1,13 @@
 import Bai1 from "./bai1"
 import Bai2 from "./bai2"
 import Bai3 from "./bai3"
+import Bai4 from "./bai4"
 const DLTOTAL =
     [
         Bai1,
         Bai2,
-        Bai3
+        Bai3,
+        Bai4
     ]
 export default DLTOTAL
 
