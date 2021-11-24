caf38a30856ed7158d723b6aaed2d3706ee969cf
Dienpham <dienpham187294@gmail.com>
2021-11-24 22:19:06 +0700

    add 1.5

--- util/filedulieu1/DulieuThuchanh_hung/DulieuTotal.js
+++ util/filedulieu1/DulieuThuchanh_hung/DulieuTotal.js
@@ -3,9 +3,10 @@ import Bai11 from "./bai11"
 import Bai12 from "./bai12"
 import Bai13 from "./bai13"
 import Bai14 from "./bai14"
+import Bai15 from "./bai15"
 const DLTOTAL =
     [
-        Bai11, Bai12, Bai13, Bai14
+        Bai11, Bai12, Bai13, Bai14, Bai15
     ]
 export default DLTOTAL
 
