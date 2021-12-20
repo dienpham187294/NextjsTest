b1c0dcaa1375a02695aed1d178bf92ca64819a99
Dienpham <dienpham187294@gmail.com>
2021-12-20 21:08:48 +0700

    add

--- util/filedulieu1/Dulieuthuchanh_MA/Dulieuinside/MA1.js
+++ util/filedulieu1/Dulieuthuchanh_MA/Dulieuinside/MA1.js
@@ -1,64 +1,107 @@
 export default {
 
-    "ENVN":
-        [
-            {
-                "EN": "Mergers and acquisitions can result in new organizations whose financial and strategic options are much improved. ",
-                "VN": "Việc sáp nhập và mua lại có thể tạo ra các tổ chức mới có các lựa chọn tài chính và chiến lược được cải thiện nhiều."
-            },
-            {
-                "EN": "External growth occurs when a business purchases the existing assets of another entity through a merger.",
-                "VN": "Tăng trưởng bên ngoài xảy ra khi một doanh nghiệp mua các tài sản hiện có của một thực thể khác thông qua sáp nhập."
-            },
-            {
-                "EN": "Besides the growth aspect, a merger may reduce risk through diversification. ",
-                "VN": "Bên cạnh khía cạnh tăng trưởng, việc sáp nhập có thể giảm thiểu rủi ro thông qua đa dạng hóa."
-            },
-            {
-                "EN": "The three common ways of joining two or more companies are a merger, consolidation, or a holding company.",
-                "VN": "Ba cách phổ biến để tham gia hai hoặc nhiều công ty là sáp nhập, hợp nhất hoặc công ty mẹ."
-            },
-            {
-                "EN": "In a merger, two or more companies are combined into one, where only the acquiring company retains its identity.",
-                "VN": "Trong một vụ sáp nhập, hai hoặc nhiều công ty được kết hợp thành một, trong đó chỉ có công ty mua lại giữ được bản sắc của mình."
-            },
-            {
-                "EN": "Mergers are legally straightforward because there is usually a single bidder and payment is made primarily with stock. ",
-                "VN": "Việc sáp nhập diễn ra đơn giản về mặt pháp lý vì thường chỉ có một nhà thầu duy nhất và việc thanh toán được thực hiện chủ yếu bằng cổ phiếu."
-            },
-            {
-                "EN": "The shareholders of each merging firm involved are required to vote to approve the merger. However, a merger of the operations of two firms may ultimately result from the acquisition of stock. ",
-                "VN": "Các cổ đông của mỗi công ty nhận sáp nhập có liên quan phải biểu quyết để thông qua việc sáp nhập. Tuy nhiên, sự hợp nhất hoạt động của hai công ty cuối cùng có thể là kết quả của việc mua lại cổ phiếu."
-            },
-            {
-                "EN": "With a consolidation, two or more companies combine to create a new company. ",
-                "VN": "Với sự hợp nhất, hai hoặc nhiều công ty kết hợp để tạo ra một công ty mới."
-            },
-            {
-                "EN": "None of the consolidation firms legally survive.",
-                "VN": "Không có công ty hợp nhất nào tồn tại hợp pháp."
-            },
-            {
-                "EN": "A holding company possesses voting control of one or more other companies.",
-                "VN": "Công ty mẹ có quyền kiểm soát biểu quyết đối với một hoặc nhiều công ty khác."
-            },
-            {
-                "EN": "The holding company comprises a group of businesses, each operating as a separate entity. By possessing more than 50% of the voting rights through common stock, the holding company has effective control of another company with a smaller percent of ownership.",
-                "VN": "Công ty mẹ bao gồm một nhóm các doanh nghiệp, mỗi doanh nghiệp hoạt động như một thực thể riêng biệt. Bằng cách sở hữu hơn 50% quyền biểu quyết thông qua cổ phiếu phổ thông, công ty mẹ có quyền kiểm soát hiệu quả đối với một công ty khác có tỷ lệ sở hữu nhỏ hơn."
-            },
-            {
-                "EN": "This occurs when a company combines with a supplier or customer. ",
-                "VN": "Điều này xảy ra khi một công ty kết hợp với một nhà cung cấp hoặc khách hàng."
-            },
-            {
-                "EN": "This occurs when two companies in a similar business combine. ",
-                "VN": "Điều này xảy ra khi hai công ty trong một doanh nghiệp tương tự kết hợp với nhau."
-            },
-            {
-                "EN": "This occurs when two companies in unrelated industries combine, such as where an electronics company joins with an insurance company. ",
-                "VN": "Điều này xảy ra khi hai công ty trong các ngành không liên quan kết hợp với nhau, chẳng hạn như trường hợp một công ty điện tử liên kết với một công ty bảo hiểm."
-            }
-        ],
+    "ENVN": [
+        {
+            "EN": "Mergers and acquisitions can result in new organizations whose financial and strategic options are much improved. ",
+            "VN": "Việc sáp nhập và mua lại có thể tạo ra các tổ chức mới có các lựa chọn tài chính và chiến lược được cải thiện nhiều."
+        },
+        {
+            "EN": "They are driven by globalization, a long-term market, various barriers to growth, which make M&As a valuable tool by which companies can quickly attempt to increase revenue.",
+            "VN": "Chúng được thúc đẩy bởi toàn cầu hóa, một thị trường dài hạn, nhiều rào cản khác nhau đối với tăng trưởng, khiến M & As trở thành một công cụ có giá trị mà các công ty có thể nhanh chóng cố gắng tăng doanh thu."
+        },
+        {
+            "EN": "External growth occurs when a business purchases the existing assets of another entity through a merger. ",
+            "VN": "Tăng trưởng bên ngoài xảy ra khi một doanh nghiệp mua các tài sản hiện có của một thực thể khác thông qua sáp nhập."
+        },
+        {
+            "EN": "You are often required to appraise the suitability of a potential merger as well as participate in negotiations. ",
+            "VN": "Bạn thường được yêu cầu thẩm định tính phù hợp của một thương vụ sáp nhập tiềm năng cũng như tham gia vào các cuộc đàm phán."
+        },
+        {
+            "EN": "Besides the growth aspect, a merger may reduce risk through diversification. ",
+            "VN": "Bên cạnh khía cạnh tăng trưởng, việc sáp nhập có thể giảm thiểu rủi ro thông qua đa dạng hóa."
+        },
+        {
+            "EN": "The three common ways of joining two or more companies are a merger, consolidation, or a holding company. ",
+            "VN": "Ba cách phổ biến để tham gia hai hoặc nhiều công ty là sáp nhập, hợp nhất hoặc công ty mẹ."
+        },
+        {
+            "EN": "In a merger, two or more companies are combined into one, where only the acquiring company retains its identity. ",
+            "VN": "Trong một vụ sáp nhập, hai hoặc nhiều công ty được kết hợp thành một, trong đó chỉ có công ty mua lại giữ được bản sắc của mình."
+        },
+        {
+            "EN": "Generally, the larger of the two companies is the acquirer. ",
+            "VN": "Nói chung, công ty lớn hơn trong số hai công ty là bên mua lại."
+        },
+        {
+            "EN": "A merger is a business combination in which the acquiring firm absorbs a second firm, and the acquiring firm remains in business as a combination of the two merged firms. ",
+            "VN": "Sáp nhập là một sự kết hợp kinh doanh trong đó công ty mua lại hấp thụ một công ty thứ hai và công ty mua lại vẫn hoạt động kinh doanh như một sự kết hợp của hai công ty được hợp nhất."
+        },
+        {
+            "EN": "The acquiring firm usually maintains its name and identity. ",
+            "VN": "Công ty mua lại thường duy trì tên và danh tính của mình."
+        },
+        {
+            "EN": "Mergers are legally straightforward because there is usually a single bidder and payment is made primarily with stock. ",
+            "VN": "Việc sáp nhập diễn ra đơn giản về mặt pháp lý vì thường chỉ có một nhà thầu duy nhất và việc thanh toán được thực hiện chủ yếu bằng cổ phiếu."
+        },
+        {
+            "EN": "The shareholders of each merging firm involved are required to vote to approve the merger. ",
+            "VN": "Các cổ đông của mỗi công ty nhận sáp nhập có liên quan phải biểu quyết để thông qua việc sáp nhập."
+        },
+        {
+            "EN": "However, merger of the operations of two firms may ultimately result from an acquisition of stock. ",
+            "VN": "Tuy nhiên, việc sáp nhập hoạt động của hai công ty cuối cùng có thể là kết quả của việc mua lại cổ phiếu."
+        },
+        {
+            "EN": "With a consolidation, two or more companies combine to create a new company. ",
+            "VN": "Với sự hợp nhất, hai hoặc nhiều công ty kết hợp để tạo ra một công ty mới."
+        },
+        {
+            "EN": "None of the consolidation firms legally survive. ",
+            "VN": "Không có công ty hợp nhất nào tồn tại hợp pháp."
+        },
+        {
+            "EN": "For example, companies A and B give all their assets, liabilities, and stock to the new company, C, in return for C’s stock, bonds, or cash. ",
+            "VN": "Ví dụ: các công ty A và B giao tất cả tài sản, nợ phải trả và cổ phiếu của họ cho công ty mới, C, để đổi lấy cổ phiếu, trái phiếu hoặc tiền mặt của C."
+        },
+        {
+            "EN": "A holding company possesses voting control of one or more other companies. ",
+            "VN": "Công ty mẹ có quyền kiểm soát biểu quyết đối với một hoặc nhiều công ty khác."
+        },
+        {
+            "EN": "The holding company comprises a group of businesses, each operating as a separate entity. ",
+            "VN": "Công ty mẹ bao gồm một nhóm các doanh nghiệp, mỗi doanh nghiệp hoạt động như một thực thể riêng biệt."
+        },
+        {
+            "EN": "By possessing more than 50% of the voting rights through common stock, the holding company has effective control of another company with a smaller percent of ownership. ",
+            "VN": "Bằng cách sở hữu hơn 50% quyền biểu quyết thông qua cổ phiếu phổ thông, công ty mẹ có quyền kiểm soát hiệu quả đối với một công ty khác có tỷ lệ sở hữu nhỏ hơn."
+        },
+        {
+            "EN": "Depending on the intent of the combination, there are three common ways in which businesses get together so as to obtain advantages in their markets. ",
+            "VN": "Tùy thuộc vào mục đích của sự kết hợp, có ba cách phổ biến mà các doanh nghiệp kết hợp với nhau để đạt được lợi thế trên thị trường của họ."
+        },
+        {
+            "EN": "Vertical merger. This occurs when a company combines with a supplier or customer. ",
+            "VN": "Hợp nhất theo chiều dọc. Điều này xảy ra khi một công ty kết hợp với một nhà cung cấp hoặc khách hàng."
+        },
+        {
+            "EN": "An example is when a wholesaler combines with retailers. ",
+            "VN": "Một ví dụ là khi một nhà bán buôn kết hợp với các nhà bán lẻ."
+        },
+        {
+            "EN": "Horizontal merger. This occurs when two companies in a similar business combine. ",
+            "VN": "Sự hợp nhất theo chiều ngang. Điều này xảy ra khi hai công ty trong một doanh nghiệp tương tự kết hợp với nhau."
+        },
+        {
+            "EN": "An example is the combining of two airlines. ",
+            "VN": "Một ví dụ là sự kết hợp của hai hãng hàng không."
+        },
+        {
+            "EN": "Conglomerate merger. This occurs when two companies in unrelated industries combine, such as where an electronics company joins with an insurance company.  ",
+            "VN": "Sáp nhập tập đoàn. Điều này xảy ra khi hai công ty trong các ngành không liên quan kết hợp với nhau, chẳng hạn như trường hợp một công ty điện tử liên kết với một công ty bảo hiểm."
+        }
+    ],
     "QSAW": [
         {
             "QS": "What can mergers and acquisitions result in new organizations?",
