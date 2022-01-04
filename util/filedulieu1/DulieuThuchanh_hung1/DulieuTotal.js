43fd0bda4a7a83e9afa813d9322d27159ae2d728
Dienpham <dienpham187294@gmail.com>
2022-01-04 14:14:50 +0700

    add

--- util/filedulieu1/DulieuThuchanh_hung1/DulieuTotal.js
+++ util/filedulieu1/DulieuThuchanh_hung1/DulieuTotal.js
@@ -1,9 +1,17 @@
 import Bai1 from "./bai1"
 import Bai2 from "./bai2"
+import Bai21 from "./bai2_1"
+import Bai22 from "./bai2_2"
+import Bai23 from "./bai2_3"
+import Bai24 from "./bai2_4"
+import Bai25 from "./bai2_5"
+
+
 import Bai3 from "./bai3"
 const DLTOTAL =
     [
-        Bai1, Bai2, Bai3
+        Bai1, Bai2, Bai3,
+        Bai21, Bai22, Bai23, Bai24, Bai25
     ]
 export default DLTOTAL
 
